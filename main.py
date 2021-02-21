409b72c92dcbf2deb709bec8b206717e8f2b5693
awilson-ppi <79424246+awilson-ppi@users.noreply.github.com>
2021-02-21 14:09:45 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Parse the server_list.json file and output the list of owners and the server that belongs to each.
+
+def parse_server_list():
+  pass
+
+
+if __name__ == '__main__':
+    parse_server_list()
